436d6d54d6a1172a0f67ea38e670908553112831
Avishai Taashur <avishai@avishai84.com>
2018-12-01 15:08:43 -0800

    context in progress

--- src/Library.js
+++ src/Library.js
@@ -2,7 +2,36 @@ import React, { Component } from 'react';
 import './Library.scss';
 import SVGinDepth from './SVGinDepth';
 
+
 class Library extends Component {
+
+constructor(props){
+  super(props);
+
+  this.setState({
+    scroll:''
+  });
+this.scroller = this.scroller.bind(this);
+}
+
+
+
+componentDidMount(){
+  window.addEventListener('scroll',  this.scroller);
+}
+componentWillUnmount(){
+  window.removeEventListener('scroll',  this.scroller);
+  console.log('event removed');
+}
+
+scroller(){
+
+  this.setState({
+    scroll:  console.log('window inner height: '+window.innerHeight +'- document scroll top'+ document.documentElement.scrollTop +'- offset entire doc height '+  document.documentElement.offsetHeight)
+  });
+
+ 
+}
 
   render() {
     return (
