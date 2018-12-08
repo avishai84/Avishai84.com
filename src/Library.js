e8f14446936f37f653b15b30726d5d28c89b708e
Avishai Taashur <avishai@avishai84.com>
2018-12-07 21:39:21 -0800

    working context

--- src/Library.js
+++ src/Library.js
@@ -3,40 +3,71 @@ import './Library.scss';
 import SVGinDepth from './SVGinDepth';
 
 
+const LoadingIcon = ({name}) => (
+  <div className="spinIcon" text={name}></div>
+ );
+const textInfo = 'More content to come soon...';
+ 
+
 class Library extends Component {
 
 constructor(props){
   super(props);
 
   this.setState({
-    scroll:''
+    scroll:'',
+    isLoading:false,
+    text:''
   });
-this.scroller = this.scroller.bind(this);
+  this.scroller = this.scroller.bind(this);
 }
 
 
 
 componentDidMount(){
-  window.addEventListener('scroll',  this.scroller);
+  this.setState({
+    scroll: window.addEventListener('scroll',  this.scroller)
+  });
+  
 }
 componentWillUnmount(){
-  window.removeEventListener('scroll',  this.scroller);
+  this.setState({
+    scroll: window.removeEventListener('scroll',  this.scroller)
+  });
   console.log('event removed');
 }
+  // window.removeEventListener('scroll',  this.scroller);
+
+
 
 scroller(){
 
-  this.setState({
-    scroll:  console.log('window inner height: '+window.innerHeight +'- document scroll top'+ document.documentElement.scrollTop +'- offset entire doc height '+  document.documentElement.offsetHeight)
-  });
+  // this.setState({
+  //   scroll:  console.log('window inner height: '+window.innerHeight +'- document scroll top'+ document.documentElement.scrollTop +'- offset entire doc height '+  document.documentElement.offsetHeight)
 
- 
+  // });
+  // add more content at the end of the page
+  if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+    this.setState({
+      isLoading: true
+     // text: document.querySelector('.library').append(textInfo)
+    });
+  }else{
+    this.setState({
+      isLoading: false
+      //text:''
+  
+    });
+  }
+
+  
 }
 
   render() {
     return (
       <div className="library">
         <SVGinDepth/>
+        {this.isLoading?<LoadingIcon className="spinner"/>:''}
       </div>
     );
   }
