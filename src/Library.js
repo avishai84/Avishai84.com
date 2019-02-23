ff31f2c20aee6273b753b90f4e65da5b0d86fb6e
Avishai Taashur <avishai@avishai84.com>
2019-02-22 21:05:16 -0800

    working on scroll

--- src/Library.js
+++ src/Library.js
@@ -3,11 +3,17 @@ import './Library.scss';
 import SVGinDepth from './SVGinDepth';
 import ConvertSVGTutorial from './ConvertSVGTutorial';
 
-const LoadingIcon = () => (
-  <div className="spinIcon"></div>
- );
+// const LoadingIcon = () => (
+//   <div className="spinIcon"></div>
+//  );
+const videosUrl = ['https://www.youtube.com/embed/w21iO-FyYhA','https://www.youtube.com/embed/IAmcCrETKIc','https://www.youtube.com/embed/8AKT8PQnh_U'];
+
+
+const videoShell = (urlPath) =>(
+  `<iframe class="videoCollection" width="560" height="315" src=${urlPath} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+);
 const textInfo = 'More content to come soon...';
-const foo = document.querySelector('.library').append(<LoadingIcon/>);
+// const foo = document.querySelector('.library').append(<LoadingIcon/>);
 
 class Library extends Component {
 
@@ -18,17 +24,39 @@ constructor(props){
     scroll:'',
     isLoading:false,
     text:'',
-    spin: ''
+    spin: '',
+    count: 0
   });
   this.scroller = this.scroller.bind(this);
 }
 
+// fetch
+// formData
+// ajaxJquery
 
+
+scroller(e){
+
+  console.log(window.innerHeight + document.documentElement.scrollTop);
+  // add more content at the end of the page
+  if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+ 
+    this.setState({
+  //  how to check if video already on the page?
+  
+      c: console.log('count vid collec.'+ this.count),
+      text: videosUrl.map((video) => (document.querySelector('.library').insertAdjacentHTML('beforeend',videoShell(video)))),
+      count: document.querySelectorAll('.videoCollection').length
+      
+    })
+  
+  }
+}
 
 componentDidMount(){
   this.setState({
-    scroll: window.addEventListener('scroll',  this.scroller),
-    spin: this.foo
+    scroll: window.addEventListener('scroll',  this.scroller)
+    //spin: 'spinning...'
   });
   console.log('DOM ready ');
 }
@@ -42,37 +70,37 @@ componentWillUnmount(){
 
 
 
-scroller(){
+// scroller(){
 
-  // this.setState({
-  //   scroll:  console.log('window inner height: '+window.innerHeight +'- document scroll top'+ document.documentElement.scrollTop +'- offset entire doc height '+  document.documentElement.offsetHeight)
+//   // this.setState({
+//   //   scroll:  console.log('window inner height: '+window.innerHeight +'- document scroll top'+ document.documentElement.scrollTop +'- offset entire doc height '+  document.documentElement.offsetHeight)
 
-  // });
-  // add more content at the end of the page
-  if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
-    this.setState({
-      isLoading: true,
-      // text: document.querySelector('.library').append(textInfo),
-      // spin:  document.querySelector('.library .spinner').style.display = 'block'
-    });
-  }else{
-    this.setState({
-      isLoading: false,
-      spin: console.log(document.querySelector('.library'))
-      //text:''
+//   // });
+//   // add more content at the end of the page
+//   if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+//     this.setState({
+//       isLoading: true,
+//       // text: document.querySelector('.library').append(textInfo),
+//       // spin:  document.querySelector('.library .spinner').style.display = 'block'
+//     });
+//   }else{
+//     this.setState({
+//       isLoading: false,
+//       //spin: console.log(document.querySelector('.library'))
+//       //text:''
   
-    });
-  }
+//     });
+//   }
 
   
-}
+// }
 
   render() {
     return (
       <div className="library">
         <SVGinDepth/>
         
-        {this.isLoading ? this.spin : 'not loading'}
+        {/* {this.isLoading ? this.spin : 'not loading'} */}
        
       </div>
     );
