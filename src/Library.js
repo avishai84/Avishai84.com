6d4ab998ed4a5b54d55f704e313d00469c5c06ac
Avishai Taashur <avishai@avishai84.com>
2019-02-26 21:03:40 -0800

    compressing html

--- src/Library.js
+++ src/Library.js
@@ -18,64 +18,7 @@ const previousHeight = [];
 const videosUrl = ['https://www.youtube.com/embed/w21iO-FyYhA','https://www.youtube.com/embed/IAmcCrETKIc','https://www.youtube.com/embed/8AKT8PQnh_U'];
 
 
-const convertSVGTutorial = `<div class="svgTextTutorial">
-<div class="container TextSvg">
-    <div class="row">
-        <div class="page-header">
-            <h1 class="screenreader">Convert Text to SVG</h1>
-        </div>
-        <div class="well">
-            <div class="wrapper">
-                <h2>Steps to Convert Text to SVG:</h2>
-                <small>Prepared By Avishai for Avishai</small>
-                <div>
-                    <ol>
-                        <li>
-                            <p class="full half">Import your PSD into Adobe Illustrator</p>
-                            <img class="container" src=${Illustration1} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">Select the entire text, hide all background images</p>
-                            <img class="container" src=${Illustration2} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">Make sure the SVG document is coded to scale.</p>
-                            <p class="full half">Go to Object -> Artboards -> Fit to Selected Art</p>
-                            <img class="container" src=${Illustration3} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">Go to "Type" --> "Create Outlines"</p>
-                            <img class="container" src=${Illustration4} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">Select the following in the SVG(svg) sub menus:</p>
-                            <img class="container" src=${Illustration5} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">SVG(svg) sub menu setup:</p>
-                            <p>Fonts: type: "Convert to Outlines"; Options: Image location: "Embed";
-                                Advanced Options: CSS Properties: "Style Attributes"; click on SVG Code.</p>
-                            <img class="container" src=${Illustration6} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">SVG Code- copy the code and as it as a text file</p>
-                            <img class="container" src=${Illustration7} alt="image" />
-                        </li>
-                        <li>
-                            <p class="full half">Font Note: Illustrator must have the same fonts as your
-                                Photoshop file. Otherwide it would replace it with default fonts.</p>
-                        </li>
-                    </ol>
-                </div>
-                <h3>The results SVG:</h3>
-                <div class="full half relative center">
-                    <img class="imgTog" src=${Illustration9} alt="bg image" />
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>`;
+const convertSVGTutorial = `<div class="svgTextTutorial"><div class="container TextSvg"> <div class="row"> <div class="page-header"> <h1 class="screenreader">Convert Text to SVG</h1> </div> <div class="well"> <div class="wrapper"> <h2>Steps to Convert Text to SVG:</h2> <small>Prepared By Avishai for Avishai</small> <div> <ol> <li> <p class="full half">Import your PSD into Adobe Illustrator</p> <img class="container" src=${Illustration1} alt="image" /> </li> <li> <p class="full half">Select the entire text, hide all background images</p> <img class="container" src=${Illustration2} alt="image" /> </li> <li> <p class="full half">Make sure the SVG document is coded to scale.</p> <p class="full half">Go to Object -> Artboards -> Fit to Selected Art</p> <img class="container" src=${Illustration3} alt="image" /> </li> <li> <p class="full half">Go to "Type" --> "Create Outlines"</p> <img class="container" src=${Illustration4} alt="image" /> </li> <li> <p class="full half">Select the following in the SVG(svg) sub menus:</p> <img class="container" src=${Illustration5} alt="image" /> </li> <li> <p class="full half">SVG(svg) sub menu setup:</p> <p>Fonts: type: "Convert to Outlines"; Options: Image location: "Embed"; Advanced Options: CSS Properties: "Style Attributes"; click on SVG Code.</p> <img class="container" src=${Illustration6} alt="image" /> </li> <li> <p class="full half">SVG Code- copy the code and as it as a text file</p> <img class="container" src=${Illustration7} alt="image" /> </li> <li> <p class="full half">Font Note: Illustrator must have the same fonts as your Photoshop file. Otherwide it would replace it with default fonts.</p> </li> </ol> </div> <h3>The results SVG:</h3> <div class="full half relative center"> <img class="imgTog" src=${Illustration9} alt="bg image" /> </div> </div> </div> </div></div></div>`;
 // video factory
 const videoShell = (urlPath) =>(
   `<iframe class="videoCollection" width="560" height="315" src=${urlPath} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
